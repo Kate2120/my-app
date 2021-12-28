b3533beec80177362f0edd4d22a088087c46880b
Kate <gifos.kate@gmail.com>
2021-12-28 07:08:09 +0200

    bags

--- src/anotherComponents/Posts.tsx
+++ src/anotherComponents/Posts.tsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import {dishes, menus} from '../components/Data/data';
 import Post from './Post';
+interface Info{
+    title: string;
+    description: string;
+    category: string;
+    image:string;
+}
+interface Dishes{
+    [key: string]: Info[];
+}
+let allDishes: Dishes = {dishes};
 function Posts() {
     let category: string = "";
     document.addEventListener('click',function (event){
         if(event.target){
-            category = ((event.target as Element).innerHTML).toString();
+            category = (event.target as Element).innerHTML;
+            let arr: Info[] = allDishes[category];
             return (
-                {dishes[category].map(post => {
+                arr.map(post => {
                     <Post post={post} />
-                })}
+                })
             );
         } else {
             if(!event.target){
                 for(let categoryName in dishes){
                     return (
-                    dishes[categoryName].map(post => {
-                        <Post post={post} />
-                    })
+                        {dishes[categoryName].map(post => {
+                        <Post />
+                    })}
                     );
                 }
             }
         }
      
        
-    });
+    })
 
  
 };
