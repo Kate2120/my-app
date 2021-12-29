dd32f6597a950d98d51ec2a7352d1f8fba4e0127
Kate <gifos.kate@gmail.com>
2021-12-29 09:04:50 +0200

    исправление ошибок

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header/header';
-import Category from './components/Body/Category';
 import Posts from './components/Body/Posts';
 import Footer from './components/Footer/footer';
 
@@ -8,7 +7,7 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <Header/>
-      <Category/>
+
       <Posts/>
       <Footer/>
 
--- src/components/Body/Posts.tsx
+++ src/components/Body/Posts.tsx
@@ -1,48 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from './Post';
 import './body.css';
-import { JsxElement } from 'typescript';
+import {Dish} from "../Data/data";
 import {dishes, categorys} from '../Data/data';
-
-
 function Posts(): JSX.Element {
-    let checkedCategory: string = "";
-    let idCategory: number | null = null;
-    let checkedDish: string = "";
-    document.addEventListener('click',function (event){
-        if(event.target){
-            if((event.target as Element).tagName === "LI"){
-            checkedCategory = (event.target as Element).innerHTML;
-            } else {
-                if((event.target as Element).tagName === "SPAN"){
-                    checkedDish = (event.target as Element).innerHTML;
-                    }
-            }
-        }
-    });
-    if(checkedCategory !== ""){
-         for (let category of categorys){
-             if(checkedCategory === category.title){
-                idCategory = category.id;
-             }
-         }
-                for(let dish of dishes){
-                    if(dish.categoryId === idCategory){
-                        return (
-                            <div className = "container">
-                            <Post image = {dish.image} title = {dish.title} description = {dish.description}/>
-                            </div>
-                        )
+let [chekkedDishes, filter] = useState<Dish[]>([]);
+let [currentDishes, change] = useState(dishes);
+    let sortDish = function() {
+        document.addEventListener('click', function (event) {
+            if (event.target) {
+                if ((event.target as Element).tagName === "LI") {
+                    let checkedCategory = (event.target as Element).innerHTML;
+                    for (let category of categorys) {
+                        if (checkedCategory === category.title) {
+                            let idCategory = category.id;
+                            chekkedDishes = [];
+                            for(let checkedCategory of dishes){
+                                if(checkedCategory.categoryId === idCategory){
+                                    chekkedDishes.push(checkedCategory);
+                                    filter(chekkedDishes);
+                                    currentDishes = [];
+                                    change(currentDishes);
+                                }
+                            }
+                        }
                     }
                 }
+                else {
+                    if ((event.target as Element).tagName === "SPAN") {
+                        let checkedDish = (event.target as Element).innerHTML;
+                        for (let checkedCategory of dishes) {
+                            chekkedDishes = [];
+                            if (checkedCategory.title === checkedDish) {
+                                chekkedDishes.push(checkedCategory);
+                                filter(chekkedDishes);
+                                currentDishes = [];
+                                change(currentDishes);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
-        return (
-            <div className = "container">
-            {dishes.map(dish => {
-               return (<Post image = {dish.image} title = {dish.title} description = {dish.description}/>)
-           })}
-           </div>
-        ); 
+  return (
+          <div>
+              <div className="menu">
+                  <ul>
+                      {categorys.map(category => {
+                          return (
+                              <li onClick={sortDish}>{category.title}</li>
+                          )
+                      })}
+                  </ul>
+              </div>
+              <div className = "container">
+                    {currentDishes.map(dish => {
+                        return (<Post image={dish.image} title={dish.title} description={dish.description}/>)
+                    })}
+                  {chekkedDishes.map(dish => {
+                      return (<Post image={dish.image} title={dish.title} description={dish.description}/>)
+                  })}
+              </div>
+          </div>
+        );
 }
 
 export default Posts;
